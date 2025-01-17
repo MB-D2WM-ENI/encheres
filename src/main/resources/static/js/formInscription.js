3a643de9d123ce128452bceff9d50a17f0c288f3
POECMelissa <153495114+POECMelissa@users.noreply.github.com>
2025-01-17 17:14:08 +0100

    Add files via upload

--- src/main/resources/static/js/formInscription.js
+++ src/main/resources/static/js/formInscription.js
@@ -6,43 +6,57 @@
  */
 
 
-const confirmationMdp = document.getElementById("confirmationMdp").value;
 let messageValidation = document.getElementById("messageValidation");
 const btnCreerProfil = document.getElementById("btnCreerProfil");
 const formSuscribe = document.getElementById("formSuscribe");
+const pseudo = document.getElementById("pseudo");
+const email = document.getElementById("email");
+const prenom = document.getElementById("prenom");
+const nom = document.getElementById("nom");
+const telephone = document.getElementById("telephone");
+const rue = document.getElementById("rue");
+const codePostal = document.getElementById("codePostal");
+const ville = document.getElementById("ville");
+const password = document.getElementById("motDePasse");
+const passwordConfirmation = document.getElementById("confirmationMdp");
+
 
 messageValidation.classList.remove('error');
+
 
 const regexMdp = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/);
 
 
-// Ecouteur d'événement sur le bouton Créer qui vérifie en front :
-// le pattern du mot de passe et la correspondance
-
-btnCreerProfil.addEventListener("click", (event) => {
+formSuscribe.addEventListener("submit", (event) => {
 
     let motDePasse = document.getElementById("motDePasse").value;
-    console.log(motDePasse);
+    let confirmationMdp = document.getElementById("confirmationMdp").value;
 
-    event.preventDefault();
+    // Vérification du mot de passe et de sa confirmation
+    if (motDePasse !== "" && confirmationMdp !== "") {
+        if (motDePasse !== confirmationMdp) {
+            event.preventDefault();
+            messageValidation.classList.add("error");
+            messageValidation.textContent = "Les deux mots de passe ne correspondent pas";
+            setTimeout(() => messageValidation.textContent = "", 5000);
+            return;
+        }
 
-    if (!regexMdp.test(motDePasse)) {
-        console.log(motDePasse);
+        // Vérification du format du mot de passe avec regex
+        if (!regexMdp.test(motDePasse)) {
+            event.preventDefault();
+            messageValidation.classList.add("error");
+            messageValidation.innerHTML = "Veuillez saisir un mot de passe avec : un chiffre, une minuscule, une majuscule, un caractère spécial et entre 8 et 20 caractères";
+            setTimeout(() => messageValidation.textContent = "", 6000);
+            return;
+        }
+    } else {
+        // Si le mot de passe ou la confirmation sont vides
+        event.preventDefault();
         messageValidation.classList.add("error");
-        messageValidation.innerHTML = "Veuillez saisir un mot de passe avec : un chiffre, une minuscule, une majuscule, un caractère spécial et entre 8 et 20 caractères";
-        setTimeout(function () {
-            messageValidation.innerHTML = "";
-        }, 6000);
+        messageValidation.textContent = "Veuillez entrer un mot de passe et sa confirmation.";
+        setTimeout(() => messageValidation.textContent = "", 5000);
+        return;
     }
-
-    if (motDePasse !== confirmationMdp) {
-        console.log(motDePasse);
-        messageValidation.classList.add("error");
-        messageValidation.textContent = "Les deux mots de passe ne correspondent pas";
-        setTimeout(function () {
-            messageValidation.innerHTML = "";
-        }, 5000);
-    }
-
-
 });
+
